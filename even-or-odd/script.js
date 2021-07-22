728cdc9b38b677d30061a00a959ee8c30baccce5
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2021-07-22 20:28:38 -0300

    adicionando desafio even or odd

---
+++ even-or-odd/script.js
@@ -0,0 +1,13 @@
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+  
+
+console.log(even_or_odd(3));
+console.log(even_or_odd(10));
+console.log(even_or_odd(29));
+console.log(even_or_odd(34));
